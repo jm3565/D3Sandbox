d8897b298a6a75f7af479df151b6621296b5c7d1
jm3565 <jnmunoz74@gmail.com>
2015-11-10 15:08:19 -0500

    adding a function to scale the chart

--- barchart/app.js
+++ barchart/app.js
@@ -4,9 +4,25 @@ var h1 = d3.select("body")
             .append("h1")
             .text("Hello World!");
 
-d3.select("body").append("div").attr("class", "chart")
+// This function is going to use a linear scale to eliminate the
+// harcoded 10 value given to scale the widths of the divs that compose the graph
+// Here we are saying that the specified max with of the chart will be 420
+var _scale = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0,300]);
+
+
+// This is going to create the chart
+d3.select("body")
+    .append("div").attr("class", "chart")
     .selectAll("div")
-        .data(data).enter().append("div")
-            .style("width", function(d){return d * 10 + "px";})
-                .text(function(d){return d;});
+        .data(data)
+        .enter()
+        .append("div")
+            .style("width", function(d){
+                return _scale(d) + "px";
+            })
+            .text(function(d){
+                return d;
+            });
 
