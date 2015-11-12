00a8d5c9ce2c3bbb0374a21f36561955d891cb8c
jm3565 <jnmunoz74@gmail.com>
2015-11-12 14:43:38 -0500

    groups1

--- concepts/app.js
+++ concepts/app.js
@@ -1,35 +1,60 @@
 data = [
-    {'name': 'A', value: 5},
-    {'name': 'B', value: 10},
-    {'name': 'C', value: 12},
-    {'name': 'D', value: 6},
-    {'name': 'E', value: 5},
-    {'name': 'F', value: 9},
-    {'name': 'G', value: 2},
-    {'name': 'H', value: 4}
+    [
+        {'name': 'A', value: 5},
+        {'name': 'B', value: 10}
+    ]
+    ,
+    [
+        {'name': 'C', value: 12},
+        {'name': 'D', value: 6}
+    ]
 ];
+
+
+//    ,
+//    {'name': 'C', value: 12},
+//    {'name': 'D', value: 6},
+//    {'name': 'E', value: 5},
+//    {'name': 'F', value: 9},
+//    {'name': 'G', value: 2},
+//    {'name': 'H', value: 4}
+
+// Get an array with the max values of each group
+function getMaxVals(data){
+    var mArr = [];
+    for(var i = 0; i < data.length; i++){
+        var g = data[i];
+        var m = d3.max(g, function(c){
+            return +c.value;
+        });
+        mArr.push(m);
+    }
+    return mArr;
+}
 
 var _heightUnit = 10,
     _widthUnit = 20,
     _depthUnit = 5,
     _addedChartHeight = 10,
     _addedChartWidth = 15,
-    _maxColHeight = d3.max(data, function(d){
-        return +d.value * _heightUnit;
+    _groupSpacing = 10;
+    _maxColHeight = d3.max(getMaxVals(data), function(m){
+        return +m * _heightUnit;
     }),
+    _totalCols = data[0].length * data.length, // Assumes there is an equal amount of columns in each group
     _chartHeight = _maxColHeight + _addedChartHeight,
-    _chartWidth = _widthUnit * data.length + _addedChartWidth;
+    _chartWidth = _widthUnit * _totalCols + _addedChartWidth;
 
 // constructs a new linear scale with range and domain of [0,1]
 var _scaleY = d3.scale.linear();
-
-//Set the domain of a scale (Y coordinate)
-_scaleY.domain([0, d3.max(data, function(d){
-    return +d.value;
+//
+////Set the domain of a scale (Y coordinate)
+_scaleY.domain([0, d3.max(getMaxVals(data), function(d){
+    return +d;
 })]);
 
-_scaleY.range([d3.max(data, function(d){
-    return +d.value * _heightUnit;
+_scaleY.range([d3.max(getMaxVals(data), function(d){
+    return +d * _heightUnit;
 }), 0]);
 
 function _getPointsPolyTop(colValue){
@@ -62,31 +87,71 @@ var chart = d3.select("body")
     .attr("width", _chartWidth)
     .attr("height", _chartHeight);
 
-var bar = chart.selectAll("g")
-    .data(data)
-    .enter()
-    .append("g")
-    .attr("transform", function(d, i){
-        return "translate(" + i * _widthUnit + ",0)";
-    });
+function renderGroup(data){
+//    var group = chart.append("div");
+//    var bar = group.selectAll("g")
+    console.log(chart.selectAll("g"));
 
-bar.append("rect")
-    .attr("y", function(d){
-        return _scaleY(d.value) + _addedChartHeight;
-    })
-    .attr("height", function(d){
-        return _chartHeight - _scaleY(d.value) - _addedChartHeight;
-    })
-    .attr("width", _widthUnit - 1);
+    var bar = chart.selectAll("g")
+            .data(data)
+            .enter()
+            .append("g")
+            .attr("transform", function(d, i){
+                return "translate(" + i * _widthUnit + ",0)";
+            });
 
-bar.append("polygon")
-    .attr("points", function(d){
-        pts = _getPointsPolyTop(d.value);
-        return pts.join(" ");
-});
+    bar.append("rect")
+        .attr("y", function(d){
+            console.log(d);
+            return _scaleY(d.value) + _addedChartHeight;
+        })
+        .attr("height", function(d){
+            return _chartHeight - _scaleY(d.value) - _addedChartHeight;
+        })
+        .attr("width", _widthUnit - 1);
+}
 
-bar.append("polygon")
-    .attr("points", function(d){
-        pts = _getPointsPolySide(d.value);
-        return pts.join(" ");
-});
+function render(){
+    for(var i = 0; i < data.length; i++){
+        console.log('renderGroup ' + i);
+        renderGroup(data[i]);
+        console.log('finished rendering group: ' + i);
+    }
+}
+
+render();
+
+
+//var bar = chart.selectAll("g")
+//    .data()
+//    .enter()
+//    .append("g")
+//    .attr("transform", function(d){
+//
+//
+//        console.log(d);
+//        console.log(i);
+//        return "translate(" + i * _widthUnit + ",0)";
+//    });
+//
+//bar.append("rect")
+//    .attr("y", function(d){
+//        console.log(d);
+//        return _scaleY(d) + _addedChartHeight;
+//    })
+//    .attr("height", function(d){
+//        return _chartHeight - _scaleY(d) - _addedChartHeight;
+//    })
+//    .attr("width", _widthUnit - 1);
+//
+//bar.append("polygon")
+//    .attr("points", function(d){
+//        pts = _getPointsPolyTop(d);
+//        return pts.join(" ");
+//});
+//
+//bar.append("polygon")
+//    .attr("points", function(d){
+//        pts = _getPointsPolySide(d);
+//        return pts.join(" ");
+//});
